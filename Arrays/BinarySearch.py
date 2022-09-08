079ca2744c5640fb2e130b9084206b2acdea0558
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-09-08 23:03:57 +0530

    Create BinarySearch.py

---
+++ Arrays/BinarySearch.py
@@ -0,0 +1,14 @@
+def binarysearch(self, arr, n, k):
+		# code here
+		low = 0
+		high = n-1
+		while(low<=high):
+		    mid = (low+high)//2
+		    if(arr[mid] == k):
+		        return mid
+		    elif(arr[mid]<k):
+		        low = mid+1 
+		    else:
+		        high = mid-1
+		return -1
+
