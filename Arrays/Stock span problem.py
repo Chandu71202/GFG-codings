96801a5a9df3d61f5315ee5071d493c453f4341d
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2022-10-11 21:40:53 +0530

    Create Stock span problem.py

---
+++ Arrays/Stock span problem.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to calculate the span of stockâ€™s price for all n days.
+    def calculateSpan(self,a,n):
+        #code here
+        s = []      
+        ans = []
+        for i in range(0,n):
+              
+            while(s != [] and a[s[-1]] <= a[i]):
+                s.pop()
+              
+            if(s == []):
+                ans.append(i+1)
+              
+            else:
+                top = s[-1]
+                ans.append(i - top)
+              
+            s.append(i)
+          
+        return ans
+                
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nikhil Kumar Singh
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        obj = Solution()
+        ans = obj.calculateSpan(a, n);
+        for i in range(n):
+            print(ans[i],end=" ")
+        print()            
+# } Driver Code Ends
